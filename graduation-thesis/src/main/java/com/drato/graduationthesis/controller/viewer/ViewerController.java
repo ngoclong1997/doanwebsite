46ed2c0c9949763675e2211aedeee29f6760cb78
Long Nguyen <nguyenngoclong@hou.edu.vn>
2019-11-07 14:44:44 +0700

    - adding chart

--- graduation-thesis/src/main/java/com/drato/graduationthesis/controller/viewer/ViewerController.java
+++ graduation-thesis/src/main/java/com/drato/graduationthesis/controller/viewer/ViewerController.java
@@ -1,13 +1,15 @@
 package com.drato.graduationthesis.controller.viewer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/viewer")
 public class ViewerController {
     @RequestMapping({"/", ""})
-    public String index() {
-        return "viewer/index";
+    public String index(Model model) {
+        model.addAttribute("title", "Xem thống kê");
+        return "viewer/chart/index";
     }
 }
